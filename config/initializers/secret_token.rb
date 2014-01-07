f3c2b1dae0de0aaaf943741521ed4b8213e6a999
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-01-07 14:13:54 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fauxgledrive::Application.config.secret_key_base = 'ac02313f02e36d8c6eee651c2c21489f76e5521a4aacddbd85edfb953e935a8c018fdd3292df965ea49fad30b918ec8f10761abf1a51f97625ce4957167ab0c2'
