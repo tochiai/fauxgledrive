39d9cb8d20c0a47577a900a4f461aa12b0557672
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-01-22 21:57:54 -0800

    Added validations for stuff\!

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -1,8 +1,17 @@
 class UserFile < ActiveRecord::Base
-	def self.from_path(file_path)
-		new(
-			local_path: file_path,
-			name: Pathname.new(file_path).basename.to_s,
-		)
-	end
+  validates :local_path, presence: true
+  validates :name, presence: true
+  validate :file_exists, if: :local_path
+
+  def self.from_path(file_path)
+    new(
+      local_path: file_path,
+      name: Pathname.new(file_path).basename.to_s,
+    )
+  end
+
+  private
+  def file_exists
+    errors.add(:local_path, "File does not exist") unless File.exists?(local_path)
+  end
 end
--- spec/models/user_file_spec.rb
+++ spec/models/user_file_spec.rb
@@ -1,14 +1,61 @@
 require "spec_helper"
 
 describe UserFile do
-	let(:basename) { "fart.txt" }
-	let(:local_path) { Rails.root.join("spec", "fixtures", basename) }
-	let(:file) { File.open(local_path) }
+  let(:basename) { "fart.txt" }
+  let(:local_path) { Rails.root.join("spec", "fixtures", basename) }
+  let(:file) { File.open(local_path) }
 
-	describe "self.from_path" do
-		subject { UserFile.from_path(local_path) }
+  describe "validations" do
+    subject { described_class.new(params) }
 
-		its(:local_path) { should == local_path }
-		its(:name) { should == basename }
-	end
+    context "without local path" do
+      let(:params) { {name: "butt"} }
+
+      it "is invalid" do
+        subject.should_not be_valid
+      end
+
+      it "gets errors" do
+        subject.should have(1).error_on(:local_path)
+      end
+    end
+
+    context "without name" do
+      let(:params) { {local_path: "/buuty/buuty"} }
+
+      it "requires name" do
+        subject.should_not be_valid
+      end
+
+      it "gets errors" do
+        subject.should have(1).error_on(:name)
+      end
+    end
+
+    context "without valid file in local path" do
+      let(:params) { {local_path: "/not_real/butt", name: "buuty"} }
+
+      it "requires a real-ass file to be at the end of that path, fool" do
+        subject.should_not be_valid
+      end
+
+      it "gets errors" do
+        subject.should have(1).error_on(:local_path)
+      end
+    end
+
+    context "when things are right and chill" do
+      let(:params) { {local_path: "/dev/null", name: "buuty"} }
+      it "is valid" do
+        subject.should be_valid
+      end
+    end
+  end
+
+  describe "self.from_path" do
+    subject { UserFile.from_path(local_path) }
+
+    its(:local_path) { should == local_path }
+    its(:name) { should == basename }
+  end
 end
